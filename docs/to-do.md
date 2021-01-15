ffca255cbbf28c220002f6c011b83eed7d925880
Peter Kello <kellsoon@gmail.com>
2021-01-15 05:42:24 +0100

    in progress

--- docs/to-do.md
+++ docs/to-do.md
@@ -2,6 +2,8 @@
 
 1. [ ] async handler
 2. [ ] files upload
-3. [ ] error handling
+3. [ ] error handling api/views
 4. [ ] logger
-5. [ ] change colored things to npm colors
+5. [ ] change colored things to npm colors + log
+6. [ ] global paths
+7. [ ] documentation
--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 // Node core modules
-const path = require("path");
+const path = require('path');
 
 // Initialize express app
-const express = require("express"),
-    app = express();
+const express = require('express'),
+  app = express();
 
 // Setting of global variables
-require(path.join(__dirname, "config", "global-variables"));
+require(path.join(__dirname, 'config', 'global-variables'));
 
 // 3rd party modules
-const { yellow } = require("chalk");
+const { yellow } = require('chalk');
 
 // Middleware modules
-const bodyParser = require("body-parser"),
-    logger = require("morgan"),
-    corsStop = require(__cors_stop),
-    errorHandler = require(__error_handler);
+const logger = require('morgan'),
+  corsStop = require(__cors_stop),
+  errorHandler = require(__error_handler);
 
 // functions
 const messages = require(__messages);
@@ -24,35 +23,35 @@ const messages = require(__messages);
 require(__config_env);
 
 // Setting template system - EJS
-app.set("view engine", "ejs");
-app.set("views");
+app.set('view engine', 'ejs');
+app.set('views');
 
 // ------------- MIDDLEWARE ------------
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger("dev"));
+app.use(express.json());
+app.use(logger('dev'));
 app.use(require(__set_multer));
 app.use(corsStop);
 
 // Routes
-const mainRoutes = require(path.join(__routes, "main")),
-    apiRoutes = require(path.join(__routes, "api", "main"));
+const mainRoutes = require(path.join(__routes, 'main')),
+  apiRoutes = require(path.join(__routes, 'api', 'main'));
 
 // Set static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Environment variables
 const PORT = process.env.PORT || 5000;
-const IP = process.env.IP || "localhost";
+const IP = process.env.IP || 'localhost';
 
 // Main routes
-app.use("/api", apiRoutes);
-app.use("/", mainRoutes);
+app.use('/api', apiRoutes);
+app.use('/', mainRoutes);
 
 // Error handler
 app.use(errorHandler);
 
 // Server listen
 app.listen(PORT, IP, () => {
-    console.log(messages.successFirstMethod(`server started on ${yellow(IP)}:${yellow(PORT)}...`));
+  console.log(messages.successFirstMethod(`server started on ${yellow(IP)}:${yellow(PORT)}...`));
 });
